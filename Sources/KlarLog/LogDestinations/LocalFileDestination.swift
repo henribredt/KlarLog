90496006a238658e9a670d201b6c6d5071958213
Henri Bredt <hello@henribredt.de>
2025-10-25 22:51:53 +0200

    adjust logs file path

--- Sources/KlarLog/LogDestinations/LocalFileDestination.swift
+++ Sources/KlarLog/LogDestinations/LocalFileDestination.swift
@@ -154,7 +154,7 @@ private actor FileOperationActor {
     private let maxMessages: Int
     
     init(fileURL: URL, maxMessages: Int) {
-        self.fileURL = fileURL
+        self.fileURL = fileURL.appending(path: "app.logs")
         self.maxMessages = maxMessages
     }
     
